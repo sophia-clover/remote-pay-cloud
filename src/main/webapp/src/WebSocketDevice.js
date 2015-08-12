5ddc8bd709223a343ec910e9bc2f40a3b94fdeac
mike hampton <mike@clover.com>
2015-08-11 18:13:36 -0600

    Example of printing an image from the cloud/lan adaptors.

--- src/main/webapp/src/WebSocketDevice.js
+++ src/main/webapp/src/WebSocketDevice.js
@@ -430,3 +430,37 @@ WebSocketDevice.prototype.sendPrintText = function(textLines) {
     var lanMessage = this.messageBuilder.buildPrintText(payload);
     this.sendMessage(lanMessage);
 }
+
+/**
+ * Send a message to ask the device if it is there.
+ * @param textLines - an  array of strings
+ */
+WebSocketDevice.prototype.sendPrintImage = function(img) {
+    var payload = {"png" : getBase64Image(img) };
+    var lanMessage = this.messageBuilder.buildPrintImage(payload);
+    this.sendMessage(lanMessage);
+}
+
+/**
+ * @private
+ * @param img
+ * @returns {string}
+ */
+function getBase64Image(img) {
+    // Create an empty canvas element
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    // Copy the image contents to the canvas
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    // Get the data-URL formatted image
+    // Firefox supports PNG and JPEG. You could check img.src to
+    // guess the original format, but be aware the using "image/jpg"
+    // will re-encode the image.
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
