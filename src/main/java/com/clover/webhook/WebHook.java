7f5e0b82b91bb22ab0c73053ac87eb45c339d0b9
mike hampton <mike@clover.com>
2015-08-24 14:05:50 -0600

    Added webhook code with bean for clarity

--- src/main/java/com/clover/webhook/WebHook.java
+++ src/main/java/com/clover/webhook/WebHook.java
@@ -1,18 +1,22 @@
 package com.clover.webhook;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by michaelhampton on 8/21/15.
  */
 public class WebHook extends javax.servlet.http.HttpServlet {
+
+  private Gson gson = new Gson();
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     doPost(request,response);
@@ -30,6 +34,21 @@ public class WebHook extends javax.servlet.http.HttpServlet {
 
     System.out.println("The payload was: '" + payload + "'");
 
+    WebHookMessage webHookEvent = gson.fromJson(payload, WebHookMessage.class);
+
+    System.out.println("The application that sent the event was: '" + webHookEvent.getAppId() + "'");
+    Iterator<String> keys =  webHookEvent.getMerchants().keySet().iterator();
+    while( keys.hasNext() ) {
+      String merchantId = keys.next();
+      List<WebHookMessage.Update> updates = webHookEvent.getMerchants().get(merchantId);
+      System.out.println("  updates for merchant: '" + merchantId + "'");
+      for(int updateIndex=0;updateIndex<updates.size();updateIndex++) {
+        WebHookMessage.Update update = updates.get(updateIndex);
+        System.out.println("    update["+updateIndex+"].objectId: '" + update.getObjectId() + "'");
+        System.out.println("    update["+updateIndex+"].type: '" + update.getType() + "'");
+        System.out.println("    update["+updateIndex+"].ts: '" + update.getTs() + "'");
+      }
+    }
     response.getWriter().write("The payload was: '" + payload + "'");
   }
 }
